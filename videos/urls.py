1385a89965be8f2ae186a8e3dbfeac9755d46940
alex <oleksiy.shamray@gmail.com>
2015-09-21 12:44:18 +0300

    Add 'rent' and 'return' URLs

--- videos/urls.py
+++ videos/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.VideoList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/rent$', views.VideoRent.as_view()),
+    url(r'^(?P<pk>[0-9]+)/return$', views.VideoReturn.as_view()),
+    url(r'^(?P<keyword>.*)$', views.VideoList.as_view()),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics, filters
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from .models import Video
 from .serializers import VideoSerializer
 
@@ -10,3 +12,32 @@ class VideoList(generics.ListCreateAPIView):
     serializer_class = VideoSerializer
     filter_backends = (filters.OrderingFilter,)
     ordering_fields = '__all__'
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+        keyword = self.kwargs['keyword']
+        return Video.objects.filter(title__icontains=keyword)
+
+
+class AbstractVideoView(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+    def post(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data={'status': self.status}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class VideoRent(AbstractVideoView):
+    status = Video.RENTED
+
+
+class VideoReturn(AbstractVideoView):
+    status = Video.RETURNED
