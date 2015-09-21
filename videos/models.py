ae08e52d864340a06383e86dd1e75a61744beaf4
alex <oleksiy.shamray@gmail.com>
2015-09-21 11:13:45 +0300

    Create Video model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    NEW = 1
+    RENTED = 2
+    RETURNED = 3
+    STATUS_CHOICES = (
+        (NEW, 'New'),
+        (RENTED, 'Rented'),
+        (RETURNED, 'Returned'),
+    )
+    title = models.CharField(max_length=128)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES, default=NEW)
