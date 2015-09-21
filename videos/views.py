96b3150a0276b4218733bf9ea4c04a215ba33e95
alex <oleksiy.shamray@gmail.com>
2015-09-21 11:45:15 +0300

    Videos app: add serializers and views

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticated
+from .models import Video
+from .serializers import VideoSerializer
 
-# Create your views here.
+
+class VideoList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = '__all__'
